831e1d72bcc50f295947288cf8560781cf3fb300
Sandeshstha0 <sandeshstha519@gmail.com>
2025-11-12 08:37:08 +0545

    chore: update dependencies and add video type definitions
    - Added @ffmpeg-installer/ffmpeg and next-video to dependencies in package.json
    - Included video.d.ts for video type definitions in tsconfig.json
    - Created video.d.ts to reference next-video types

--- app/components/Home/VideoSection.jsx
+++ app/components/Home/VideoSection.jsx
@@ -4,7 +4,7 @@ export default function VideoSection() {
   return (
     <div style={{ position: "relative", paddingBottom: "56.25%", height: 0 }}>
       <iframe
-        src="https://www.youtube.com/embed/rj9HWOo0SSE"
+        src="https://www.youtube.com/embed/rj9HWOo0SSE?autoplay=1&mute=1&rel=0&showinfo=0&controls=1&modestbranding=1"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
