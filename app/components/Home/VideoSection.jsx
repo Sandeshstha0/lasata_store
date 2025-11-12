615328d67d9f0821eee76c7ead839899578624da
Sandeshstha0 <sandeshstha519@gmail.com>
2025-11-12 06:43:21 +0545

    feat: add VideoSection component with YouTube iframe integration
    - Added VideoSection component to render a responsive YouTube video iframe.
    - Updated tsconfig.json to include the new VideoSection component for type checking.
    - Added react-iframe and react-player dependencies to package.json.
    - Removed unused video file: SajiloSnacks Tvc Ads.mp4.

---
+++ app/components/Home/VideoSection.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+export default function VideoSection() {
+  return (
+    <div style={{ position: "relative", paddingBottom: "56.25%", height: 0 }}>
+      <iframe
+        src="https://www.youtube.com/embed/rj9HWOo0SSE"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="YouTube Video"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      ></iframe>
+    </div>
+  );
+}
